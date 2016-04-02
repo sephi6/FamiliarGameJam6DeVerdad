93c7e7e720422b1183ab197d8fdd69a12685aeae
sephi6 <vdominguezaragon@gmail.com>
2016-04-02 20:51:31 +0200

    menu principal

---
+++ Assets/Scripts 1/UIManagerMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIManagerMenu : MonoBehaviour {
+
+    public Button abrirHow;
+    public Button cerrarHow;
+
+    public Animator panelHow;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void abreHow()
+    {
+        panelHow.SetInteger("cierra", 1);
+        
+    }
+
+    public void cierraHow()
+    {
+        panelHow.SetInteger("cierra", 0);
+    }
+
+    public void cargaJuego()
+    {
+        Application.LoadLevel(1);
+    }
+}
